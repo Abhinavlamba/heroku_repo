92296ff4ca66cf8419bd1ad1f54bf5f64e3f441d
Rohit (Linux) <torohithill@gmail.com>
2019-09-25 12:15:23 -0700

    Moved fibonacci function to other file

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 from flask import Flask
+from mylib.mat import fib as f
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Home page success."
-def f(n):
-    if n <= 1:
-        return 1
-    else:
-        return f(n-1) + f(n-2)
-
 @app.route('/fib/<int:n>')
 def fib(n):
     return str(f(n))
